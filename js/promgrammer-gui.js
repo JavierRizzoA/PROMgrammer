a8d2f622190aab3f0e790f4764dbbe447e8eef4b
Javier Rizzo <javierrizzoa@gmail.com>
2015-07-15 11:20:00 -0700

    Add selection to bytes

--- js/promgrammer-gui.js
+++ js/promgrammer-gui.js
@@ -1,15 +1,30 @@
 var PROMGRAMMER = PROMGRAMMER || {};
 
+/*
+ * To do when the window is resized.
+ */
 $(window).resize(function(){
-    $('#editor').css('height', $(window).height() - 87);
-    $('#toolbar').css('height', $(window).height() - 87);
+    $('#editor').css('height', $(window).height() - 82);
+    $('#toolbar').css('height', $(window).height() - 82);
 });
 
+/**
+ * @funciton
+ * Loads an array of bytes to the editor.
+ * @param {number[]} bytes - The array of bytes to load.
+ * @todo Remove all children from the editor first.
+ */
 PROMGRAMMER.displayBytes = function(bytes) {
     var offset = 0;
     for(i = 0; i < bytes.length; i++) {
-        $('<span class="byte">' + PROMGRAMMER.decToHex(bytes[i], 2) + '</span>').appendTo('#hex');
-        $('<span class="charByte">' + PROMGRAMMER.charCode(bytes[i]) + '</span>').appendTo('#map');
+        if( i > 0) {
+            $('<span class="byte" data-byte="' + i + '" onclick="PROMGRAMMER.editing = PROMGRAMMER.selectByte(' + i + ', true, PROMGRAMMER.editing)">' + PROMGRAMMER.decToHex(bytes[i], 2) + '</span>').appendTo('#hex');
+            $('<span class="charByte" data-byte="' + i + '" onclick="PROMGRAMMER.editing = PROMGRAMMER.selectByte(' + i + ', false, PROMGRAMMER.editing)">' + PROMGRAMMER.byteToChar(bytes[i]) + '</span>').appendTo('#map');
+        } else {
+            $('<span class="byte selected cursor" data-byte="' + i + '" onclick="PROMGRAMMER.editing = PROMGRAMMER.selectByte(' + i + ', true, PROMGRAMMER.editing)">' + PROMGRAMMER.decToHex(bytes[i], 2) + '</span>').appendTo('#hex');
+            $('<span class="charByte selected" data-byte="' + i + '" onclick="PROMGRAMMER.editing = PROMGRAMMER.selectByte(' + i + ', false, PROMGRAMMER.editing)">' + PROMGRAMMER.byteToChar(bytes[i]) + '</span>').appendTo('#map');
+            this.editing = {b: 0, hex: true};
+        }
         if((i + 1) % 16 == 1) {
             $('<div>' + PROMGRAMMER.decToHex(offset, 4, true) + '</div>').appendTo('#offset');
         } else if((i + 1) % 16 == 0) {
@@ -20,4 +35,42 @@ PROMGRAMMER.displayBytes = function(bytes) {
         }
         offset++;
     }
+};
+
+/**
+ * @function
+ * Selects the byte that was clicked and unselects the previously selected one.
+ * @param {number} b - The selected byte number.
+ * @param {boolean} hex - Wether the selection is on the hex editor (true) or the character map (false).
+ * @param {Object} previouSelection - The previously selected byte.
+ * @param {number} previousSelection.b - The byte number.
+ * @param {boolean} previousSelection.hex - Wether the the selection was on the hex editor (true) or the character map (false).
+ * @returns {Object} newSelection - The new selected byte.
+ * @returns {number} newSelection.b - The new selected byte number.
+ * @returns {boolean} newSelection.hex - Wether the new selection is on the hex editor (true) or the character map (false).
+ */
+PROMGRAMMER.selectByte = function(b, hex, previousSelection) {
+    /*
+     * Deselect the previous selection.
+     */
+    if(previousSelection.hex) {
+        $('span.byte[data-byte="' + previousSelection.b + '"]').toggleClass("selected cursor");
+        $('span.charByte[data-byte="' + previousSelection.b + '"]').toggleClass("selected");
+    } else {
+        $('span.byte[data-byte="' + previousSelection.b + '"]').toggleClass("selected");
+        $('span.charByte[data-byte="' + previousSelection.b + '"]').toggleClass("selected cursor");
+    }
+
+    /*
+     * Select the clicked byte.
+     */
+    if(hex) {
+        $('span.byte[data-byte="' + b + '"]').toggleClass("selected cursor");
+        $('span.charByte[data-byte="' + b + '"]').toggleClass("selected");
+    } else {
+        $('span.byte[data-byte="' + b + '"]').toggleClass("selected");
+        $('span.charByte[data-byte="' + b + '"]').toggleClass("selected cursor");
+    }
+
+    return {b: b, hex: hex};
 };
--- js/promgrammer.js
+++ js/promgrammer.js
@@ -1,23 +1,62 @@
 var PROMGRAMMER = PROMGRAMMER || {};
 
+/*
+ * How many addresses (bytes) has the EEPROM.
+ * 8192 as the default, for the 28C64.
+ */
 PROMGRAMMER.addresses = 8192;
+
+/*
+ * An array representation of all the bytes of the EEPROM.
+ */
 PROMGRAMMER.bytes = [];
 
-PROMGRAMMER.newFile = function() {
-    this.bytes = [];
-    for(i = 0; i < this.addresses; i++) {
-        this.bytes.push(00);
+/*
+ * Which byte is being edited.
+ * b = the number of the byte being edited (-1 being none).
+ * hex = wether it is being edited in hex mode (true) or map mode (false).
+ */
+PROMGRAMMER.editing = {b: -1, hex: true};
+
+/**
+ * @function
+ * Creates a new array of bytes.
+ * @param {number} addresses - The length of the array (8192 by default).
+ * @param {number} defaultValue - The value to initialize each item (0 by default).
+ * @returns {nuber[]} the byte array of length 'addresses' and values 'defaultValue'.
+ */
+PROMGRAMMER.newFile = function(addresses, defaultValue) {
+    addresses = typeof addresses !== 'undefined' ? addresses : 8192;
+    defaultValue = typeof defaultValue !== 'undefined' ? defaultValue : 0;
+    var bytes = [];
+    for(i = 0; i < addresses; i++) {
+        bytes.push(defaultValue);
     }
+    return bytes;
 };
 
-PROMGRAMMER.charCode = function(b) {
+/**
+ * @function
+ * Converts a number to its ASCII char, or to '.' if the char is not printable.
+ * @param {number} b - The byte to convert.
+ * @returns {string} The ASCII representation of b, or '.' if b is not a printable character.
+ */
+PROMGRAMMER.byteToChar = function(b) {
     if(b >= 33) {
         return String.fromCharCode(b);
     } else {
-        return ".";
+        return '.';
     }
 };
 
+/**
+ * @function
+ * Convers a decimal number to hexadecimal.
+ * @param {number} dec - The number to convert.
+ * @param {number} digits - The minimum number of digits for the result; it adds 0s to the left if the number is smaller (0 by default).
+ * @param {boolean} prefix - Wether or not to add '0x' at the left (false by default).
+ * @returns {string} The hexadecimal representation of the decimal number.
+ */
 PROMGRAMMER.decToHex = function(dec, digits, prefix) {
     digits = typeof digits !== 'undefined' ? digits : 0;
     prefix = typeof prefix !== 'undefined' ? prefix : false;
@@ -25,11 +64,14 @@ PROMGRAMMER.decToHex = function(dec, digits, prefix) {
     while(hex.length < digits) {
         hex = "0" + hex;
     }
+    hex = hex.toUpperCase();
     if(prefix) return "0x" + hex; else return hex;
 };
 
-
+/*
+ * To do when the window finishes loading.
+ */
 $(window).load(function() {
-    PROMGRAMMER.newFile();
+    PROMGRAMMER.bytes = PROMGRAMMER.newFile(PROMGRAMMER.addresses);
     PROMGRAMMER.displayBytes(PROMGRAMMER.bytes);
 });
